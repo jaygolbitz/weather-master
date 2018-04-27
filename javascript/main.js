335cd3cf71253f80e178188bccb5d384250d454a
Jason Golbitz <golbitz.ja@gmail.com>
2018-04-27 10:19:26 -0400

    Initial commit

---
+++ javascript/main.js
@@ -0,0 +1,9 @@
+// ========================================
+// Your JavaScript goes here
+// ========================================
+
+function shakePic() {
+  var pic = document.querySelector(".pic");
+  pic.classList.remove("shake");
+  pic.classList.add("shake");
+}

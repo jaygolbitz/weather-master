a55ef5ee9a071dc1fa7936dde5b4e70b4e9f43fa
jaygolbitz <36082750+jaygolbitz@users.noreply.github.com>
2018-04-29 21:56:46 -0400

    Delete main.js

--- weather/javascript/main.js
+++
@@ -1,9 +0,0 @@
-// ========================================
-// Your JavaScript goes here
-// ========================================
-
-function shakePic() {
-  var pic = document.querySelector(".pic");
-  pic.classList.remove("shake");
-  pic.classList.add("shake");
-}
